636944c0401dfb180f0d490c72b2393f54196dd6
colineckert <colineckert10@gmail.com>
2021-02-20 21:24:22 -0800

    Start validations

--- app.js
+++ app.js
@@ -1,7 +1,35 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require("./models/User");
+const bodyParser = require("body-parser");
 
-app.get("/", (req, res) => res.send("Hello world!"));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const user = new User({
+    handle: "jim",
+    email: "jim@jim.jim",
+    password: "jimisgreat123"
+  })
+  user.save();
+  res.send("Hello world!")
+});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 

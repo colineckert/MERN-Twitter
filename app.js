5c2d1e7299c1e484574bde53c967aa560ce7e79a
colineckert <colineckert10@gmail.com>
2021-02-21 22:56:37 -0800

    Fix app.js path

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = express();
 const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 const passport = require('passport');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 
-const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -27,10 +33,3 @@ app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
